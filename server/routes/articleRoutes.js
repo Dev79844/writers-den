e999aa8ac4bb89e6430abc7ee633f76a88411480
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2023-06-23 12:41:58 +0530

    added: article controllers

--- server/routes/articleRoutes.js
+++ server/routes/articleRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const {isLoggedIn} = require('../middlewares/user')
-const {createArticle,getArticleBySlug,updateArticle,deleteArticle} = require('../controllers/articleControllers')
+const {createArticle,getArticleBySlug,updateArticle,deleteArticle,listArticles,favoriteArticle,unfavoriteArticle} = require('../controllers/articleControllers')
 
 const router = express.Router()
 
@@ -8,5 +8,8 @@ router.post("/",isLoggedIn,createArticle)
 router.get("/:slug",getArticleBySlug)
 router.put("/:slug",isLoggedIn,updateArticle)
 router.delete("/:slug",isLoggedIn,deleteArticle)
+router.get("/",listArticles)
+router.post("/:slug/favorite", isLoggedIn, favoriteArticle)
+router.delete("/:slug/favorite", isLoggedIn,unfavoriteArticle)
 
 module.exports = router
