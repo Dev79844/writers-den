ae298b79f64cbc52b11ba804346f82522b2ce8dc
Dev Parikh <51128342+Dev79844@users.noreply.github.com>
2023-06-23 12:59:18 +0530

    added: comment model

---
+++ server/models/comment.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const User = require('./user')
+
+const commentSchema = new mongoose.Schema({
+    body:{
+        type:String,
+        required: true
+    },
+    author:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    article:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Article'
+    }
+},{timestamps:true})
+
+commentSchema.methods.toCommentResponse = async function(user){
+    const authorObj = await User.findById(this.author)
+    return {
+        id: this._id,
+        body:this.body,
+        createdAt:this.createdAt,
+        updatedAt: this.updatedAt,
+        author: authorObj.toProfileJSON(user)
+    }
+}
+
+module.exports = mongoose.model('Comment',commentSchema)
